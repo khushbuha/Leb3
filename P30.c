13e03ab53dd8bdf05c6172b1913d54ace100b209
khushbuha <khushbuha60@gmail.com>
2025-10-17 08:48:10 +0530

    Update P30.c

--- P30.c
+++ P30.c
@@ -7,11 +7,11 @@ int main() {
     printf("Prime numbers between 1 and 500 are:\n");
 
     for(i = 2; i <= 500; i++) {
-        isPrime = 1; // assume number is prime
+        isPrime = 1; 
 
         for(j = 2; j <= i/2; j++) {
             if(i % j == 0) {
-                isPrime = 0; // not prime
+                isPrime = 0; 
                 break;
             }
         }
