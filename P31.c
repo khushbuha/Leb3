69e2305cb3dd7a19b54edad5925230c012d2a85f
khushbuha <khushbuha60@gmail.com>
2025-10-17 08:47:34 +0530

    Update P31.c

--- P31.c
+++ P31.c
@@ -6,11 +6,12 @@ int main() {
     long long sum = 0;
 
     for(i = 2; i <= 500; i++) {
-        isPrime = 1; // assume prime
+        isPrime = 1;
 
         for(j = 2; j <= i/2; j++) {
             if(i % j == 0) {
-                isPrime = 0; // not prime
+                isPrime = 0; 
+                
                 break;
             }
         }
